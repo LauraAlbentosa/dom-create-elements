39acff432c4f3f96a1d3d161304852c5720c8fa2
lauraosallor <lauraosallor@gmail.com>
2024-07-03 11:59:47 +0200

    inserción multiple

--- src/js/index.js
+++ src/js/index.js
@@ -67,28 +67,229 @@ const createDiv = () => {
   const redButton = document.createElement('button');
   const greenButton = document.createElement('button');
 
-  redButton.textContent = 'red'
-  greenButton.textContent = 'green'
+  redButton.textContent = 'red';
+  greenButton.textContent = 'green';
 
-  divWithButtons.classList.add('div')
+  divWithButtons.classList.add('div');
 
   divWithButtons.append(redButton);
   divWithButtons.append(greenButton);
   document.body.append(divWithButtons);
 
-  const red = () =>{
-    divWithButtons.classList.remove('green')
-    divWithButtons.classList.add('red')
-  }
+  const red = () => {
+    divWithButtons.classList.remove('green');
+    divWithButtons.classList.add('red');
+  };
 
-  const green = () =>{
-    divWithButtons.classList.remove('red')
-    divWithButtons.classList.add('green')
-  }
+  const green = () => {
+    divWithButtons.classList.remove('red');
+    divWithButtons.classList.add('green');
+  };
 
-  redButton.addEventListener('click', red)
-  greenButton.addEventListener('click', green)
+  redButton.addEventListener('click', red);
+  greenButton.addEventListener('click', green);
 };
 
-createDiv()
+//createDiv();
 
+//- Crea una función que sea capaz de generar 25 números aleatorios y los devuelva.
+
+
+const generateRandomNumber = () => {
+  const randomNumbers = [];
+  for (let i = 0; i < 25; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 100));
+  }
+
+  return randomNumbers
+};
+
+
+
+//console.log(randomNumbers);
+
+//- Crea una función que reciba los 25 números aleatorios que has creado en el ejercicio anterior y genere e inserte en el DOM 2 listas, una con números pares y otra con números impares.
+
+const createEvenOddList = () => {
+  const fragment = document.createDocumentFragment();
+  const evensList = document.createElement('ul');
+  const oddsList = document.createElement('ul');
+  const randomNumbers = generateRandomNumber();
+
+  randomNumbers.forEach(number => {
+    let numberList = document.createElement('li');
+    numberList.textContent = number;
+    if (number % 2 === 0) {
+      evensList.append(numberList);
+    } else {
+      oddsList.append(numberList);
+    }
+  });
+
+  fragment.append(evensList, oddsList);
+  document.body.append(fragment);
+};
+
+//createEvenOddList();
+
+/*- Con esta estructura, crea una función que, a medida que vayas escribiendo, te ponga dentro de la lista:
+  - El texto tiene X caracteres.
+  - El texto tiene X vocales.
+  - El texto tiene X consonantes.
+  - Has introducido X espacios
+*/
+
+const inputTextElement = document.getElementById('input3');
+const listElement = document.getElementById('list-input');
+
+
+
+const countTestCharactes = () => {
+  const vowels = 'aeiou';
+  let countVowels = 0;
+  let countConsonants = 0;
+  let countSpaces = 0;
+
+  const fragment = document.createDocumentFragment();
+  const charactersTotal = document.createElement('li');
+  const vowelsTotal = document.createElement('li');
+  const consonantsTotal = document.createElement('li');
+  const spacesTotal = document.createElement('li');
+
+  for (const character of inputTextElement.value) {
+    if (vowels.includes(character)) {
+      countVowels++;
+    } else if (character.charCodeAt() === 32) {
+      countSpaces++;
+    } else {
+      countConsonants++;
+    }
+  }
+
+  charactersTotal.textContent = `El texto tiene ${inputTextElement.value.length} caracteres`;
+  vowelsTotal.textContent = `El texto tiene ${countVowels} vocales`;
+  consonantsTotal.textContent = `El texto tiene ${countConsonants} consonantes`;
+  spacesTotal.textContent = `Has introducido ${countSpaces} espacios`;
+
+  listElement.textContent = ''
+  fragment.append(charactersTotal, vowelsTotal, consonantsTotal, spacesTotal);
+  listElement.append(fragment);
+};
+
+
+inputTextElement.addEventListener('input', countTestCharactes);
+
+//- Con este HTML consigue que al introducir un número POSITIVO y MAYOR de 0 se genere la tabla de multiplicar de ese número del 0 al 10 como elementos de la lista. En el caso de que el número no sea correcto o no haya número, el botón estará desactivado.
+
+const buttonNumberElement = document.getElementById('button-mult');
+const numberInputElement = document.getElementById('input-number');
+const listMultiply = document.getElementById('multiply-list');
+
+const acceptNumber = () => {
+  buttonNumberElement.disabled = false;
+  if (numberInputElement.value <= 0 ) {
+    buttonNumberElement.disabled = true;
+  }
+
+  return numberInputElement.value
+};
+
+numberInputElement.addEventListener('input', acceptNumber);
+const multiply = () => {
+  const fragment = document.createDocumentFragment()
+  const number = acceptNumber()
+
+  for (let i = 0; i<=10; i++){
+    let newElement = document.createElement('li')
+    newElement.textContent = `${number} x ${i}: ${number*i} `
+    console.log(newElement.textContent)
+    fragment.append(newElement)
+  }
+
+  listMultiply.append(fragment)
+
+};
+
+buttonNumberElement.addEventListener('click', multiply)
+
+
+//- Con este objeto debes crear tarjetas de usuario que muestren todos los datos, el diseño es libre, lo importante es que muestren toda la información del usuario y la imagen de perfil. Crea una función que genere todas las tarjetas de usuario y las inserte en el DOM
+
+
+const USERS = [
+  {
+    name: 'Josep Flores',
+    age: 77,
+    username: 'Josep85',
+    email: 'Josep_Flores@hotmail.com',
+    profileImage: 'https://randomuser.me/api/portraits/women/24.jpg'
+  },
+  {
+    name: 'Ricardo Rosas',
+    age: 43,
+    username: 'Ricardo_Rosas',
+    email: 'Ricardo_Rosas22@yahoo.com',
+    profileImage: 'https://randomuser.me/api/portraits/men/57.jpg'
+  },
+  {
+    name: 'Iván Tamayo',
+    age: 40,
+    username: 'tamayo87',
+    email: 'Ivan_Tamayo61@yahoo.com',
+    profileImage: 'https://randomuser.me/api/portraits/men/9.jpg',
+    job: 'Lead Communications Designer'
+  },
+  {
+    name: 'Maica Villanueva',
+    age: 64,
+    username: 'Maica.Villanueva18',
+    email: 'Maica.Villanueva1@yahoo.com',
+    profileImage: 'https://randomuser.me/api/portraits/women/4.jpg'
+  },
+  {
+    name: 'Pedro Estrada',
+    age: 77,
+    username: 'Pedro29',
+    email: 'Pedro_Estrada22@hotmail.com',
+    profileImage: 'https://randomuser.me/api/portraits/men/2.jpg',
+    job: 'Central Directives Liaison'
+  },
+  {
+    name: 'Jorge Cedillo',
+    age: 33,
+    username: 'Jorge_Cedillo',
+    email: 'Jorge.Cedillo2@yahoo.com',
+    profileImage: 'https://randomuser.me/api/portraits/men/88.jpg'
+  }
+];
+
+const createCard = () =>{
+  const fragment = document.createDocumentFragment()
+  for ( const user of USERS){
+    const divCard = document.createElement('div')
+    const nameElement = document.createElement('span')
+    nameElement.textContent = user.name
+    const ageElement = document.createElement('span')
+    ageElement.textContent = user.age
+    const userNameElement = document.createElement('span')
+    userNameElement.textContent = user.username
+    const emailElement = document.createElement('span')
+    emailElement.textContent = user.email
+    const profileImageElement = document.createElement('span')
+    profileImageElement.textContent = user.profileImage
+
+    divCard.classList.add('card')
+
+    divCard.append(nameElement)
+    divCard.append(ageElement)
+    divCard.append(userNameElement)
+    divCard.append(emailElement)
+    divCard.append(profileImageElement)
+
+    fragment.append(divCard)
+  }
+
+  document.body.append(fragment)
+}
+
+createCard()
